65550892e79135b4e48fa6f59919dd95c49cef9c
IanReyes44 <112287821+IanReyes44@users.noreply.github.com>
2025-02-07 20:07:19 -0500

    added rudimentary window for application startup

---
+++ Java-workspace/StartWindow.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+    
+
+public class StartWindow{
+    public static void main(String[] args) {
+       
+        JFrame startPage = new JFrame("Login");
+
+
+        startPage.setSize(600,400);
+
+        startPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        startPage.setVisible(true);
+
+    }
+}

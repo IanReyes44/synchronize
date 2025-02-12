a99fbbaee171953e8412a0915beb1018e98b8740
IanReyes44 <112287821+IanReyes44@users.noreply.github.com>
2025-02-11 20:06:38 -0800

    Added username and password feild

--- Java-workspace/StartWindow.java
+++ Java-workspace/StartWindow.java
@@ -1,17 +1,30 @@
+
+import java.awt.FlowLayout;
 import javax.swing.JFrame;
-    
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 
 public class StartWindow{
     public static void main(String[] args) {
        
-        JFrame startPage = new JFrame("Login");
+        JFrame startPage = new JFrame("Synchronize Login");
+      
+        startPage.setSize(300,400);
 
+        
+        JLabel userLabel = new JLabel("Username: ");
+        JTextField userField = new JTextField(20);
+        JLabel passLabel = new JLabel("Password: ");
+        JTextField passField = new JTextField(20);
 
-        startPage.setSize(600,400);
+        startPage.setLayout(new FlowLayout());
+        startPage.add(userLabel);
+        startPage.add(userField);
+        startPage.add(passLabel);
+        startPage.add(passField);
 
         startPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
         startPage.setVisible(true);
 
     }

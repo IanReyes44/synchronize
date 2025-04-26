eaa4906833e511ec91ff71eb66a61b445615a951
IanReyes44 <ireyes4341@csu.fullerton.edu>
2025-04-26 01:23:46 -0700

    Revert "implementing google auth"
    This reverts commit 4270bbda314e93515411b9d5070b1fd3702f3216.

--- calendar-main/src/index.js
+++ calendar-main/src/index.js
@@ -2,19 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import {createClient} from "@supabase/supabase-js";
-import {SessionContextProvider} from "@supabase/auth-helpers-react";
-const supabase = createClient(
-  "https://utshmtxzfuqsdozixrtj.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0c2htdHh6ZnVxc2Rveml4cnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NDU5MzMsImV4cCI6MjA2MTIyMTkzM30.OxivDnAjbyrhKzzhqA6_l8yrmoq8M_N37ySk-KIKhas"
-);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <SessionContextProvider supabaseClient={supabase}>
-      <App />
-    </SessionContextProvider>
+    <App />
   </React.StrictMode>,
   rootElement
 );
